c613792d2b80c6f74313f18416bfee1ed8ad252e
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-05-13 23:15:16 +0200

    V3

--- src/index.js
+++ src/index.js
@@ -26,13 +26,42 @@ class Projects {
 class Populate {
     static makeTodo(){
         const todo = document.querySelector(".todo");
-        for (const item of Todo.list){
+        todo.innerHTML = "";
+        for (let i = 0; i < Todo.list.length; i++){
             const itemDiv =  document.createElement("div");
-            itemDiv.innerHTML = `<input type="checkbox"><p>${item.title}</p><p>${item.description}</p><p>${item.dueDate}</p>`
+            itemDiv.id = `todo-item-${i}`;
+            itemDiv.innerHTML = `<input type="checkbox"><p>${Todo.list[i].title}</p><p>${Todo.list[i].description}</p><p>${Todo.list[i].dueDate}</p>`
             todo.appendChild(itemDiv);
         }     
     }
 }
+// btn logic
+const btnForm = document.getElementById("add");
+const form = document.querySelector(".form");
+const overlay = document.getElementById("overlay");
+btnForm.addEventListener("click",()=>{
+    overlay.style.display = "block";
+    form.style.display ="block";
+    
+})
+const btnBack = document.getElementById("back");
+btnBack.addEventListener("click",()=>{
+    form.style.display ="none";
+    overlay.style.display = "none";
+})
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const date = document.getElementById("date");
+    if(title.value.length){
+        const newItem = new Todo(title.value,description.value,date.value);
+    }
+})
+
+
+
+
 
 const test = new Todo("test","a test todo","date");
 console.log(test);
